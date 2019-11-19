fdd5a7c578443c6afb2bfa5ab0817e82b32f3f87
hipnotismo <martinconcetti97@gmail.com>
2019-11-19 10:47:49 -0300

    Mecanica de colicion, todavia con bugs!

--- pacman/src/Gameplay/gameplay.cpp
+++ pacman/src/Gameplay/gameplay.cpp
@@ -1,5 +1,6 @@
 #include "raylib.h"
 
+#include <iostream>
 #include "Gameplay/gameplay.h"
 #include "Menu/Menu.h"
 #include "Credits/credits.h"
@@ -21,12 +22,18 @@ namespace gradius {
 
 		Vector2 playerPosition;
 		int playerRadius;
+		Vector2 playerSpeed;
 
 		Vector2 pill1;
 		int pillRadius1;
 		bool pill1Active = true;
 
-		int poinst;
+		Rectangle block1;
+		//Vector2 block1;
+		//Vector2 blockPosition;
+
+		int poinst = 0;
+		int direction;
 
 		int Core()
 		{
@@ -50,14 +57,20 @@ namespace gradius {
 
 			InitWindow(screenWidth, screenHeight, "PACMAN");
 			
-			playerPosition = { (float)GetScreenWidth() / 2, (float)GetScreenHeight() / 2 };
+			playerPosition = { static_cast<float>(GetScreenWidth()) / 2, static_cast<float>(GetScreenHeight()) / 2 };
 			playerRadius = 15;
+			playerSpeed = {0.0f,0.0f};
 			SetTargetFPS(60);
 
 			pill1 = { 100, 100 };
 			pillRadius1 = 10;
 
-			poinst = 0;
+			block1.height = static_cast <float>(100);
+			block1.width = static_cast <float>(100);
+			block1.x = static_cast <float>(200);
+			block1.y = static_cast <float>(200);
+			//block1 = {(static_cast < float>(40),static_cast < float>(40))};
+			//blockPosition= {(static_cast < float>(200),static_cast < float>(200))};
 		}
 
 		static void Change() {
@@ -84,30 +97,54 @@ namespace gradius {
 				if (IsKeyPressed('P')) pause = !pause;
 
 				if (!pause) {
+					playerPosition.x += playerSpeed.x;
+					playerPosition.y += playerSpeed.y;
+
 					if (IsKeyDown(KEY_W)) {
+						direction = 1;
 						if (playerPosition.y > 0) {
-							playerPosition.y -= 5.0f;
+							playerSpeed.y = -5.0f;
+							playerSpeed.x = 0.0f;
 						}
 					}
 
 					if (IsKeyDown(KEY_S)) {
+						direction = 2;
 						if (playerPosition.y > 0) {
-							playerPosition.y += 5.0f;
+							playerSpeed.y = 5.0f;
+							playerSpeed.x = 0.0f;
 						}
 					}
 
 					if (IsKeyDown(KEY_A)) {
+						direction = 3;
 						if (playerPosition.x > 0) {
-							playerPosition.x -= 5.0f;
+							playerSpeed.x = -5.0f;
+							playerSpeed.y = 0.0f;
 						}
 					}
 
 					if (IsKeyDown(KEY_D)) {
+						direction = 4;
 						if (playerPosition.x > 0) {
-							playerPosition.x += 5.0f;
+							playerSpeed.x = 5.0f;
+							playerSpeed.y = 0.0f;
 						}
 					}
-
+					if (CheckCollisionCircleRec(playerPosition,playerRadius,block1)) {
+						if (direction == 1) {
+							playerPosition.y += 6.0f;
+						}
+						if (direction == 2) {
+							playerPosition.y -= 6.0f;
+						}
+						if (direction == 3) {
+							playerPosition.x += 6.0f;
+						}
+						if (direction == 4) {
+							playerPosition.x -= 6.0f;
+						}
+					}
 					if (CheckCollisionCircles(pill1, pillRadius1, playerPosition, playerRadius)) {
 						if(pill1Active==true){ poinst += 1; }					
 						pill1Active = false;
@@ -122,6 +159,7 @@ namespace gradius {
 
 			ClearBackground(RAYWHITE);
 			DrawCircleV(playerPosition, static_cast<float>(playerRadius), BLACK);
+			DrawRectangle(block1.x,block1.y,block1.width,block1.height,BLUE);
 			if (pill1Active == true) {
 				DrawCircleV(pill1, static_cast<float>(pillRadius1), GOLD);
 			}
